a5dbb4e746e7077a53360bf6624323cb188a181f
lantang-kargo <56533581+lantang-kargo@users.noreply.github.com>
2020-01-09 16:31:03 +0700

    Update upload_to_aws.sh

--- upload_to_aws.sh
+++ upload_to_aws.sh
@@ -5,4 +5,4 @@ export AWS_ACCESS_KEY_ID=`echo $CREDS | jq -r '.Credentials.AccessKeyId'`
 export AWS_SECRET_ACCESS_KEY=`echo $CREDS | jq -r '.Credentials.SecretAccessKey'`
 export AWS_SESSION_TOKEN=`echo $CREDS | jq -r '.Credentials.SessionToken'`
 
-aws s3 cp --recursive '/drone/src/images' s3://$PLUGIN_S3_BUCKET --metadata '{"Cache-Control":"no-cache", "Content-Type":"image/svg+xml"}'
+aws s3 cp --recursive '/drone/src/images' s3://$PLUGIN_S3_BUCKET --cache-control "no-cache"
